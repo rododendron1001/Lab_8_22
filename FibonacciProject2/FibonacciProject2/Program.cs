1960b2f88fbd087f2324306d2a319dee8471011b
rododendron1001 <60620826+rododendron1001@users.noreply.github.com>
2020-03-22 16:18:24 +0200

    very simple solution for string Fibonacci

--- FibonacciProject2/FibonacciProject2/Program.cs
+++ FibonacciProject2/FibonacciProject2/Program.cs
@@ -6,7 +6,45 @@ namespace FibonacciProject2
     {
         static void Main(string[] args)
         {
-            
+            int i1 = 0;
+            int i2 = 1;
+            int i;
+            Console.WriteLine("Number of elements for the string Fibonacci : ");
+
+            if (int.TryParse(Console.ReadLine(), out int n) && (n > 0))
+            {
+                switch (n)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Elements of string Fibonacci are : " + i1);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Elements of string Fibonacci are : " + i1 + "," + i2);
+                            break;
+                        }
+                    default:
+                        Console.Write("Elements of string Fibonacci are : " + i1 + "," + i2);
+                        for (int j = 3; j <= n; j++)
+                        {
+                            i = i1 + i2;
+                            i1 = i2;
+                            i2 = i;
+                            Console.Write("," + i);                           
+                        }
+                        break;
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine(" Number of elements has to be an integer !");
+            }
+            Console.ReadLine();
+
+
         }
     }
 }
