99deb08834e2252af686e527db410d355b9564a7
rododendron1001 <60620826+rododendron1001@users.noreply.github.com>
2020-03-22 16:30:48 +0200

    very simple method to obtain the string Fibonacci

--- FibonacciProject2/FibonacciProject2/Program.cs
+++ FibonacciProject2/FibonacciProject2/Program.cs
@@ -9,42 +9,49 @@ namespace FibonacciProject2
             int i1 = 0;
             int i2 = 1;
             int i;
-            Console.WriteLine("Number of elements for the string Fibonacci : ");
+            int nr = 0;
+          
+             // 3 incercari la dispozitie 
+             
+            while (nr < 3)
 
-            if (int.TryParse(Console.ReadLine(), out int n) && (n > 0))
             {
-                switch (n)
+                Console.WriteLine("Number of elements for the string Fibonacci : ");
+                if (int.TryParse(Console.ReadLine(), out int n) && (n > 0))
                 {
-                    case 1:
-                        {
-                            Console.WriteLine("Elements of string Fibonacci are : " + i1);
+                    switch (n)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Elements of string Fibonacci are : " + i1);
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.WriteLine("Elements of string Fibonacci are : " + i1 + "," + i2);
+                                break;
+                            }
+                        default:
+                            Console.Write("Elements of string Fibonacci are : " + i1 + "," + i2);
+                            for (int j = 3; j <= n; j++)
+                            {
+                                i = i1 + i2;
+                                i1 = i2;
+                                i2 = i;
+                                Console.Write("," + i);
+                            }
                             break;
-                        }
-                    case 2:
-                        {
-                            Console.WriteLine("Elements of string Fibonacci are : " + i1 + "," + i2);
-                            break;
-                        }
-                    default:
-                        Console.Write("Elements of string Fibonacci are : " + i1 + "," + i2);
-                        for (int j = 3; j <= n; j++)
-                        {
-                            i = i1 + i2;
-                            i1 = i2;
-                            i2 = i;
-                            Console.Write("," + i);                           
-                        }
-                        break;
+                    }
+                    nr = 4;
                 }
-                
+                else
+                {
+                    Console.WriteLine(" Number of elements has to be an integer !");
+                    nr++;
+                }
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
             }
-            else
-            {
-                Console.WriteLine(" Number of elements has to be an integer !");
-            }
-            Console.ReadLine();
-
-
         }
     }
 }
